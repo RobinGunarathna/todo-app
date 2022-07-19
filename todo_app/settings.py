9a70ae16ddbc4d45c0822d1ff4948a357594adae
RobinGunarathna <madgunrob@gmail.com>
2022-07-19 15:51:31 +0000

    routing done and http response for test

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo_base.apps.BaseConfig',
+    'todo_base.apps.TodoBaseConfig',
     ]
 
 MIDDLEWARE = [
