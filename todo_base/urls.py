7ccb3cb7c48793bc37765fe2a3f7ec53cf3be36b
RobinGunarathna <madgunrob@gmail.com>
2022-07-28 22:57:40 +0000

    setting up signin/out not done yet new class views.py line 10 new path urls.py line 6. soem more html

--- todo_base/urls.py
+++ todo_base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, MyLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', MyLoginView.as_view(), name = 'login'),
     path('', TaskList.as_view(), name = 'tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name = 'task'),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
