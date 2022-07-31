4a676600ade383e7ac6a4bec1dd178ea1d08d2aa
RobinGunarathna <madgunrob@gmail.com>
2022-07-31 15:41:30 +0000

    imported logoutView and path in urls.py. imported mixin in views.py and added it to TaskList class

--- todo_base/urls.py
+++ todo_base/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', MyLoginView.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name = 'logout'),
     path('', TaskList.as_view(), name = 'tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name = 'task'),
     path('task-create/', TaskCreate.as_view(), name = 'task-create'),
