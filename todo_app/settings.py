25068ae8f0b1b4faac9d01d04931aae5b64dd98a
RobinGunarathna <madgunrob@gmail.com>
2022-08-04 17:29:18 +0000

    changed debug to false

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'todo_base/templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['todo-app-p4.herokuapp.com', 'localhost']
 
